7577ea1fdf0d1426544fe4379cd508ab0897cd35
RJGMorris <60813702+RJGMorris@users.noreply.github.com>
2020-02-19 20:20:01 +0000

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.urls import resolve
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .models import Profile
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
+
+
+@login_required
+def follow(request, pk):
+    user_profile = Profile.objects.get(user=request.user)
+    to_follow_profile = Profile.objects.get(pk=pk)
+
+    user_profile.following.add(to_follow_profile.user)
+    to_follow_profile.followers.add(user_profile.user)
+
+    return redirect('/blog/user/{}/'.format(to_follow_profile.user.username))
+
+
+@login_required
+def un_follow(request, pk):
+    user_profile = Profile.objects.get(user=request.user)
+    to_un_follow_profile = Profile.objects.get(pk=pk)
+
+    user_profile.following.remove(to_un_follow_profile.user)
+    to_un_follow_profile.followers.remove(user_profile.user)
+
+    return redirect('/blog/user/{}/'.format(to_un_follow_profile.user.username))
